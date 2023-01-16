589e80d26d70ea1a56b7c70e9117c9bb880f3db4
Lmickk2 <lukemickan21@icloud.com>
2023-01-16 17:28:22 -0500

    base file/folder structure copied over

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const db = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const projData = await db.Project.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+      });
+  
+      const projects = projData.map(project => project.get({ plain: true }));
+  
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.post('/', async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
